a41a1c05bc97aa2c89b9eb0b71a6fe361ff0117c
pigeontoe <electriclady1@gmail.com>
2018-04-19 22:40:03 -0400

    make tests more specific

--- __tests__/statJs.test.js
+++ __tests__/statJs.test.js
@@ -4,25 +4,25 @@ const testDataSet = [3, 234, 23, 78, 356, 98, 67, 3, 34, 23, 94, 94, 90, 9];
 const testDataSet2 = [3, 234, 23, 78, 356, 98, 67, 3, 34, 23, 94, 94, 90, 9, 9];
 
 describe('statJs', () => {
-  test('median should return correct median', () => {
+  test('.median() should return correct median', () => {
     expect(statJs.median(testDataSet)).toBe(72.5);
     expect(statJs.median(testDataSet2)).toBe(67);
   });
 
-  test('mean should return correct mean', () => {
+  test('.mean() should return correct mean', () => {
     expect(statJs.mean(testDataSet)).toBe(86.14285714285714);
   });
 
-  test('standard deviation should return correct standard deviation', () => {
+  test('.standard() deviation should return correct standard deviation', () => {
     expect(statJs.stdDev(testDataSet)).toBe(94.81776291004395);
   });
 
-  test('mad should return correct median absolute deviation', () => {
+  test('.mad() should return correct median absolute deviation', () => {
     expect(statJs.mad(testDataSet)).toBe(44);
     expect(statJs.mad(testDataSet2)).toBe(44);
   });
 
-  test('mavWinsorize should return correctly Winsorized data set', () => {
+  test('.mavWinsorize() should return correctly Winsorized data set', () => {
     expect(statJs.mavWinsorize(testDataSet)).toEqual([ 3, 234, 23, 78, 248.5, 98, 67, 3, 34, 23, 94, 94, 90, 9 ]);
   });
 });
